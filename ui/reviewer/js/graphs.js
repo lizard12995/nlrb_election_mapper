bf19fa7f203f78d2669998f19204f4503684ee10
Sofia <sfasull1@binghamton.edu>
2023-05-10 12:53:56 -0400

    updated graphs
    got graphs to show correlation between what is being graphed and number of eligible voters per city. Depending on map symbology, graphs are shown and x axis updates

--- ui/reviewer/js/graphs.js
+++ ui/reviewer/js/graphs.js
@@ -1,8 +1,6 @@
 //idea is to make a for loop to populate a matrix with x and y values to graph
 //this would run after the data has been loaded
 
-
-
 function createDataforGraph(data,x,y) {
     const XY = [];
     for (const element of data.features) {
@@ -18,13 +16,13 @@ function createDataforGraph(data,x,y) {
 // here would make a different for loop to create frquency of a variable, so specific to histograpms
 
 //idea would be to run this after createData for graph has happened
-function createGraphs(preparedData, chartID) {
+function createGraphs(preparedData, chartID, color) {
     const myChart = new Chart(chartID, {
     type: "scatter",
     data: {
         datasets: [{
           pointRadius: 4,
-          pointBackgroundColor: "rgba(0,0,255,1)",
+          pointBackgroundColor: color,
           data: preparedData,
         }]
       },
@@ -34,37 +32,8 @@ function createGraphs(preparedData, chartID) {
 }
 
 
-function testGraph() {
-    const xyValues = [
-        {x:50, y:7},
-        {x:60, y:8},
-        {x:70, y:8},
-        {x:80, y:9},
-        {x:90, y:9},
-        {x:100, y:9},
-        {x:110, y:10},
-        {x:120, y:11},
-        {x:130, y:14},
-        {x:140, y:14},
-        {x:150, y:15}
-      ];
-      
-      const myChart = new Chart("testGraph", {
-        type: "scatter",
-        data: {
-          datasets: [{
-            pointRadius: 4,
-            pointBackgroundColor: "rgba(0,0,255,1)",
-            data: xyValues
-          }]
-        },
-        options:{}
-      });
-  return myChart;
-}
 
 export{
     createGraphs,
-    createDataforGraph,
-    testGraph
+    createDataforGraph
 }
--- ui/reviewer/js/main.js
+++ ui/reviewer/js/main.js
@@ -1,7 +1,7 @@
 import { downloadData } from './data.js';
 import { initMap} from './map.js';
 import { showCityDataInList } from './election-info.js';
-import { createDataforGraph, createGraphs, testGraph } from './graphs.js';
+import { createDataforGraph, createGraphs } from './graphs.js';
 import { clearLegend, testLegend, defaultLegend, fillLegend1, fillLegend2, fillLegend3 } from './legend.js';
 
 
@@ -13,13 +13,20 @@ let app = {
 const map = initMap();
 const radioButtons = document.querySelectorAll('.filter-radio-button');
 const legendEl = document.querySelector('.info-legend.leaflet-control');
+const xAxis = document.querySelector(".x-axis");
 
 
-function onDataLoad(data) {
-    createGraphs(createDataforGraph(data,'num_elections','tot_voted'),"myChart1");
-    }
-function graphData() {
-    downloadData(onDataLoad);
+function graphMap1(data) {
+  createGraphs(createDataforGraph(data,'tot_wins','elig_vote_pop'),"myChart","#6cae75");
+}
+function graphMap2(data) {
+  createGraphs(createDataforGraph(data,'num_elections','elig_vote_pop'),"myChart","#50a5d3");
+}
+function graphMap3(data) {
+  createGraphs(createDataforGraph(data,'voter_turnout','elig_vote_pop'),"myChart","#d8914a");
+}
+function graphMap4(data) {
+  createGraphs(createDataforGraph(data,'perc_win','elig_vote_pop'),"myChart","#cb9cf2");
 }
 
 function onMapDataLoad(data) {
@@ -51,7 +58,7 @@ function mapUnionWinsData() {
 }
 
 
-//display parks markers on map when you want
+//might be an issue with too many overlapping maps
 for (const button of radioButtons){
   button.addEventListener('click', (evt) => {
     map.dataLayer.clearLayers();
@@ -62,18 +69,26 @@ for (const button of radioButtons){
         mapData();
         clearLegend(legendEl);
         defaultLegend(legendEl);
+        downloadData(graphMap1);
+        xAxis.innerHTML = "total union wins";
       } else  if (button.id == "num-elections-button"){
         mapNumElectionsData();
         clearLegend(legendEl);
         fillLegend1(legendEl);
+        downloadData(graphMap2);
+        xAxis.innerHTML = "number of elections";
       } else if (button.id== "voter-turnout-button"){
         mapVoterTurnoutData();
         clearLegend(legendEl);
         fillLegend2(legendEl);
+        downloadData(graphMap3);
+        xAxis.innerHTML = "percent voter turnout";
       } else {
         mapUnionWinsData();
         clearLegend(legendEl);
         fillLegend3(legendEl);
+        downloadData(graphMap4);
+        xAxis.innerHTML = "percent union wins";
     }
   });
 }
@@ -94,9 +109,9 @@ function setupInteractionEvents() {
 
 mapData();
 defaultLegend(legendEl);
-graphData();
 setupInteractionEvents();
-testGraph();
+downloadData(graphMap1);
+xAxis.innerHTML = "total union wins";
 
 window.app = app;
 window.mapview = map;
